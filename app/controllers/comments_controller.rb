1b226db845fbf46daba3a5dbdb38697781625e59
oldrookie-engineer <paburofu0023@icloud.com>
2020-04-19 15:29:40 +0900

    投稿一覧表示機能を実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,21 @@
 class CommentsController < ApplicationController
 
-def index
-end
+  def index
+    @comments = Comment.all
+  end
 
-def new
-  @comment = Comment.new
-end
+  def new
+    @comment = Comment.new
+  end
 
-def create
-  @comment = params[:comment]
-  redirect 
-end
+  def create
+    Comment.create(comment_params)
+    redirect_to new_comment_path
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
 
 end
